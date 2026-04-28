5ce060d0c5bdd1d78c4e8f12bcce47238b8f83db
s <shreshthabagohil@gmail.com>
2026-04-28 13:03:03 +0000

    fix: switch map tile to dark_nolabels to remove multilingual continent labels

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -190,7 +190,7 @@ export default function MapView({ shipments, riskFilter: riskFilterProp, onFilte
         zoomControl={true}
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
           subdomains="abcd"
           maxZoom={20}
